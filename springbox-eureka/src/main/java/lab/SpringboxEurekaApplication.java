a613b1f166347cb82aec9a33a375f5cb137f44d7
Yadhav Jayaraman <yjayaraman@pivotal.io>
2016-11-13 17:32:40 -0500

    added files

--- springbox-eureka/src/main/java/lab/SpringboxEurekaApplication.java
+++ springbox-eureka/src/main/java/lab/SpringboxEurekaApplication.java
@@ -1,4 +1,4 @@
-package io.springbox.eureka;
+package lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class SpringboxEurekaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringboxEurekaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboxEurekaApplication.class, args);
+	}
 }
