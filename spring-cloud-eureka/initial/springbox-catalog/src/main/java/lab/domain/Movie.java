2a0b871f253c2de725994c6442ef1b57c212ab81
Yadhav Jayaraman <yjayaraman@pivotal.io>
2016-11-13 20:03:22 -0500

    added files

--- spring-cloud-eureka/initial/springbox-catalog/src/main/java/lab/domain/Movie.java
+++ spring-cloud-eureka/initial/springbox-catalog/src/main/java/lab/domain/Movie.java
@@ -13,57 +13,70 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "movies")
+@Table(name="movies")
 public class Movie {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
     @Column(nullable = false)
     private String title;
 
     @Column(nullable = false)
     private String mlId;
-
+	
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "movies_genres",
             joinColumns = {@JoinColumn(name = "movie_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "genre_id", referencedColumnName = "id")})
     private List<Genre> genres;
+    
+	@Column(nullable=false)
+	private int numberInStock;
 
-    @Column(nullable = false)
-    private int numberInStock;
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public String getMlId() {
+		return mlId;
+	}
 
-    public List<Genre> getGenres() {
-        return genres;
-    }
+	public void setMlId(String mlId) {
+		this.mlId = mlId;
+	}
 
-    public void setGenres(List<Genre> genres) {
-        this.genres = genres;
-    }
+	public List<Genre> getGenres() {
+		return genres;
+	}
 
-    public String getMlId() {
-        return mlId;
-    }
+	public void setGenres(List<Genre> genres) {
+		this.genres = genres;
+	}
 
-    public void setMlId(String mlId) {
-        this.mlId = mlId;
-    }
+	public int getNumberInStock() {
+		return numberInStock;
+	}
+
+	public void setNumberInStock(int numberInStock) {
+		this.numberInStock = numberInStock;
+	} 
+	
+	
+	
+	
 }
+
--- spring-cloud-eureka/initial/springbox-client/src/main/java/lab/SpringboxClientApplication.java
+++ spring-cloud-eureka/initial/springbox-client/src/main/java/lab/SpringboxClientApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringboxCatalogApplication {
+public class SpringboxClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringboxCatalogApplication.class, args);
+		SpringApplication.run(SpringboxClientApplication.class, args);
 	}
 }
