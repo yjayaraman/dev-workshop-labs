c505c2fa82848a48c29a3ecd04e711e0c4ec85cf
Yadhav Jayaraman <yjayaraman@pivotal.io>
2017-11-07 10:43:54 -0500

    added labs for dotnet modernization

---
+++ dotnettx/lab06/flyway-4.2.0/sql/V1__IntialSchema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `fortunecookies` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Cookie` longtext,
+  `Language` longtext,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
