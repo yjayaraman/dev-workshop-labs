dcaef18cfd8e2ca6cfe2ebc663d4d7b74c1479fe
Yadhav Jayaraman <yjayaraman@pivotal.io>
2017-12-18 20:12:43 -0500

    added counterservice to final

--- spring-boot-actuator/final/springbox-catalog/src/main/java/lab/repositories/GenreRepository.java
+++
@@ -1,8 +0,0 @@
-package lab.repositories;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import lab.domain.Genre;
-
-public interface GenreRepository extends PagingAndSortingRepository<Genre, Long> {
-}
