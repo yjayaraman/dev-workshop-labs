1024a7fb0b9427c983d98b8f7ea5e83da06374dd
Yadhav Jayaraman <yjayaraman@pivotal.io>
2017-12-18 19:07:03 -0500

    removed springbox catalog from basics initial

--- spring-boot-basics/initial/springbox-catalog/src/main/java/lab/repositories/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package lab.repositories;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import lab.domain.Movie;
-
-public interface MovieRepository extends PagingAndSortingRepository<Movie, Long> {
-}
